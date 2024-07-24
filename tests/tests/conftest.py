09f4a50a16d58d6766d43f42ec99d89037c2fc80
Yaroslav Petryk <ypetryk.lviv@gmail.com>
2024-07-24 14:51:16 +0300

    Rename docker compose file

--- tests/tests/conftest.py
+++ tests/tests/conftest.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-import time
 import allure
 
 
